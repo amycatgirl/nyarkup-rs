a930371e3473c6521357f356ffa3a4a571b39c2a
Amelia <amy@amogus.cloud>
2024-12-30 15:01:45 -0400

    chore: add new method and current_token method
    im not sure if i am ever going to use current_token tbh
    
    BUT i _did_ include the language's grammar

--- src/nyarkup/parser.rs
+++ src/nyarkup/parser.rs
@@ -5,6 +5,13 @@ struct Parser {
 }
 
 impl Parser {
+    fn new(token_stream: Vec<Token>) -> Self {
+        Self { token_stream }
+    }
+
+    fn current_token(&self) -> Token {
+        self.token_stream[0]
+    }
     fn peek(&self) -> Token {
         self.token_stream[1]
     }
@@ -12,7 +19,21 @@ impl Parser {
     fn pop(&mut self) {
         self.token_stream.pop();
     }
-
-    // todo)) Document grammar for recursive-desent parsing!!
     // Language design is a time-consuming task, but it's soooooooo worth it
+
+    /*
+     * [INFO] I suck at making LL(1) grammar, it's my first time
+     * So um, have it if you want to request some changes or smth
+     * D -> E
+     * E -> E'
+     * E' -> T
+     * E' -> {E'}
+     * E' -> _E'_
+     * E' -> ~E'~
+     * E' -> > E'
+     * E' -> `e`
+     * E' -> e
+     * T -> == e
+     * T -> -- e
+     */
 }
