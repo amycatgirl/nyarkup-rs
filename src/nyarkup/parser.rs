222f166cbddcc0e9a263456fde62ed663ec37df1
Amelia <amy@amogus.cloud>
2025-01-03 17:33:16 -0400

    wip: continue working on parser, implement methods

--- src/nyarkup/parser.rs
+++ src/nyarkup/parser.rs
@@ -1,24 +1,59 @@
-use crate::nyarkup::lexer::Token;
+use std::any::Any;
+
+use super::lexer::{Token, TokenType};
 
 struct Parser {
     token_stream: Vec<Token>,
+    cursor: usize,
 }
 
 impl Parser {
     fn new(token_stream: Vec<Token>) -> Self {
-        Self { token_stream }
+        Self {
+            token_stream,
+            cursor: 0,
+        }
     }
 
-    fn current_token(&self) -> Token {
-        self.token_stream[0]
-    }
-    fn peek(&self) -> Token {
-        self.token_stream[1]
+    fn parse_document(&mut self) -> bool {
+        self.parse_expression() && self.expect_token(TokenType::EOF)
     }
 
-    fn pop(&mut self) {
-        self.token_stream.pop();
+    fn parse_expression(&self) -> bool {}
+
+    fn parse_headers() -> bool {}
+
+    fn parse_formatting() -> bool {}
+
+    fn parse_bold() -> bool {}
+    fn parse_italic() -> bool {}
+    fn parse_strikethrough() -> bool {}
+    fn parse_code() -> bool {}
+
+    fn scan_token(&mut self) -> Token {
+        let current_token: Token = self.token_stream[self.cursor];
+        self.cursor += 1;
+
+        return current_token;
     }
+
+    fn putback_token(&mut self) {
+        self.cursor -= 1;
+    }
+
+    fn expect_token(&mut self, expected: TokenType) -> bool {
+        let token = self.scan_token();
+
+        if token.token_type.type_id() == expected.type_id() {
+            true
+        } else {
+            self.putback_token();
+            false
+        }
+    }
+
+    // [TODO] Errors!
+
     // Language design is a time-consuming task, but it's soooooooo worth it
 
     /*
